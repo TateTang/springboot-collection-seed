ade993ab7fed4549f61a81cd72f13965466ef03c
tangmingfu <tangmingfu@guokang.com>
2020-04-21 09:10:09 +0800

    first

---
+++ src/main/java/com/company/project/SpringbootCollectionSeedApplication.java
@@ -0,0 +1,13 @@
+package com.company.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCollectionSeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCollectionSeedApplication.class, args);
+	}
+
+}
