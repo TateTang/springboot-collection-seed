87c2c7203721b78d60f5e3a234584d6863b84a0c
tangmingfu <tangmingfu@guokang.com>
2020-04-21 18:11:56 +0800

    four 一些修改

---
+++ src/main/java/com/company/project/config/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package com.company.project.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @Author tangmf
+ * @Date 2020/4/21 16:32
+ * @Description swaager配置类
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+	@Bean
+	public Docket customImplementation() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.company.project")).build().apiInfo(testApiInfo());
+	}
+
+	private ApiInfo testApiInfo() {
+		return new ApiInfo("springboot-collection-seed", // 大标题
+				"springboot-collection-seed", // 小标题
+				"v1.0", // 版本
+				"NO terms of server", "tangmf", // 作者
+				"The Apache License, Version 2.0", // 链接显示文字
+				"http://www.apache.org/licenses/LICENSE-2.0.html"// 网站链接
+		);
+	}
+}
---
+++ src/main/java/com/company/project/core/ResultUtil.java
@@ -0,0 +1,45 @@
+package com.company.project.core;
+
+/**
+ * @Author tangmf
+ * @Date 2020/4/21 13:48
+ * @Description 响应结果生成工具
+ */
+public class ResultUtil {
+    private static final String DEFAULT_SUCCESS_MESSAGE = "SUCCESS";
+
+    public static Result success() {
+        return ResultUtil.success(null, DEFAULT_SUCCESS_MESSAGE);
+    }
+
+    public static Result success(Object data) {
+        return ResultUtil.success(data, DEFAULT_SUCCESS_MESSAGE);
+    }
+
+    private static Result success(Object data, String message) {
+        Result result = new Result();
+        result.setData(data);
+        result.setMessage(message);
+        return result;
+    }
+
+    public static Result fail(String message) {
+        return ResultUtil.fail(ResultCode.FAIL, null, message);
+    }
+
+    public static Result fail(ResultCode code, String message) {
+        return ResultUtil.fail(code, null, message);
+    }
+
+    public static Result fail(ResultCode code, Object data) {
+        return ResultUtil.fail(code, data, "");
+    }
+
+    public static Result fail(ResultCode code, Object data, String message) {
+        Result result = new Result();
+        result.setCode(code);
+        result.setData(data);
+        result.setMessage(message);
+        return result;
+    }
+}
